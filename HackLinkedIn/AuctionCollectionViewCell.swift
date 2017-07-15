85f9f9e2d30bbfbb646faa49db9c1d7d8cf15c62
Garrett O'Grady <ogradyg1@udayton.edu>
2017-07-15 08:56:30 -0700

    add data

--- HackLinkedIn/AuctionCollectionViewCell.swift
+++ HackLinkedIn/AuctionCollectionViewCell.swift
@@ -17,11 +17,11 @@ class AuctionCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var price: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
-    var placeAr = [["big cities", "Action Bike shot", "Golden Gate"],["big cities", "Action Bike shot", "Golden Gate"],["big cities", "Action Bike shot", "Golden Gate"]]
-    var companyAr = [["Apple", "Google", "Chegg"],["Apple", "Google", "Chegg"],["Apple", "Google", "Chegg"]]
-    var priceAr = [["$999", "$100", "$750"],["$999", "$100", "$750"],["$999", "$100", "$750"]]
-    var timeLeftAr = [["5 days","8 days","10 days"],["3 days","2 days","1 days"],["13 days","11 days","04 days"]]
-    var imageViewAr = [[UIImage(named:"city"),UIImage(named:"bike"),UIImage(named:"ggb")],[UIImage(named:"city"),UIImage(named:"bike"),UIImage(named:"ggb")],[UIImage(named:"city"),UIImage(named:"bike"),UIImage(named:"ggb")]]
+    var placeAr = [["Big Cities", "Action Bike shot", "Golden Gate"],["Learning in Class", "Clam Chowder", "Business Casual"],["Doctors and Medicine", "Long Distance Running", "Pair Programming"]]
+    var companyAr = [["Apple", "Google", "Salesforce"],["Citi Bank", "Campbells", "GE"],["Stanford", "Google", "Udacity"]]
+    var priceAr = [["$450", "$100", "$750"],["$500", "$100", "$300"],["$250", "$100", "$50"]]
+    var timeLeftAr = [["5 days","8 days","10 days"],["3 days","2 days","1 days"],["13 days","11 days","4 days"]]
+    var imageViewAr = [[UIImage(named:"city"),UIImage(named:"bike"),UIImage(named:"ggb")],[UIImage(named:"class"),UIImage(named:"clam"),UIImage(named:"casual")],[UIImage(named:"doc"),UIImage(named:"lr"),UIImage(named:"pair")]]
     
     func configure(row:Int, cell:Int) {
         print("configuring")
